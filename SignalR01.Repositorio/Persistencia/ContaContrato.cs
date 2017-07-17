68a06e34e2a1286712e118207d950dbfbdc71ef1
Luiz Guilherme Bandeira <arkanael@gmail.com>
2017-07-17 08:56:04 -0300

    Instalado o bootstrap e criado as views de cadastro e consulta

---
+++ SignalR01.Repositorio/Persistencia/ContaContrato.cs
@@ -0,0 +1,15 @@
+﻿using SignalR01.Entidades;
+using SignalR01.Repositorio.Contratos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR01.Repositorio.Persistencia
+{
+    public class ContaContrato : BaseRepositorio<Conta>, IContaRepositorio
+    {
+
+    }
+}
---
+++ SignalR01.Web/Controllers/ContaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalR01.Web.Controllers
+{
+    public class ContaController : Controller
+    {
+        // GET: Conta/Cadastro
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        // GET: Conta/Consulta
+        public ActionResult Consulta()
+        {
+            return View();
+        }
+    }
+}
